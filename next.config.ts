61b71003a162d8e0ca2c009ca6e94613701cff3b
Sven Daneel <sven4696@gmail.com>
2026-01-28 12:02:13 +0100

    update imports

--- next.config.ts
+++ next.config.ts
@@ -13,22 +13,6 @@ const nextConfig: NextConfig = {
   },
   // 🔐 SECURITY: CSP headers moved to src/middleware.ts for nonce-based policy
   // Middleware provides better control and per-request nonce generation
-  webpack: (config, { isServer }) => {
-    // Ignore optional dependencies that are not needed for web builds
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@react-native-async-storage/async-storage': false,
-      'pino-pretty': false,
-    };
-
-    // Suppress warnings for these optional dependencies
-    config.ignoreWarnings = [
-      { module: /node_modules\/@react-native-async-storage\/async-storage/ },
-      { module: /node_modules\/pino-pretty/ },
-    ];
-
-    return config;
-  },
 };
 
 export default nextConfig;
